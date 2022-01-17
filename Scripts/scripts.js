bf3444f646584d1e3c8c46821a29bd46733d80b2
JaxyBLU <tukebaike123@gmail.com>
2022-01-17 21:18:12 +0400

    Progress...

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,10 +1,16 @@
-const myLink = document.getElementById('hotels');
+const myLink = document.querySelectorAll('.expandable');
 const myBox = document.querySelector('.information');
 
-myLink.addEventListener('mouseover', ()=>{
-    myBox.style['visibility'] = 'visible';
-})
-
-myLink.addEventListener('mouseout', ()=>{
-    myBox.style['visibility'] = 'hidden';
+myLink.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        if(item.classList.contains('active')){
+            myLink.forEach((element) => element.classList.remove('active'));
+            myBox.classList.add('invisible');
+            return;
+        } else{
+            myLink.forEach((element) => element.classList.remove('active'));
+            item.classList.add('active');
+            myBox.classList.remove('invisible');
+        }
+    });
 })
